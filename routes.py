3add5fddf947eef64d4f24bc3a46ef5ca3b63468
Othman951 <ot.belfakir@gmail.com>
2025-05-25 16:09:32 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+import requests
+from flask import Blueprint, jsonify
 
-main = Blueprint("main", __name__)
+main = Blueprint('main', __name__)
 
-@main.route("/")
-def home():
-    return "Hello World from Flask!"
+@main.route('/dexscreener/<chain>/<pair>')
+def dexscreener_price(chain, pair):
+    url = f"https://api.dexscreener.com/latest/dex/pairs/{chain}/{pair}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Impossible de récupérer les données"}), 400
 
